702d3a9818d30f75119e315b80be5c20c8f4173a
Luke <lukeaveil@yahoo.com>
2016-08-15 10:41:11 +0100

    adds increase and decrease temp functionality

--- thermostat/spec/ThermostatSpec.js
+++ thermostat/spec/ThermostatSpec.js
@@ -9,4 +9,14 @@ describe('Thermostat', function(){
   it('starts at 20 degrees', function(){
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
+
+  it('increases the temperature with the up button', function(){
+    thermostat.increaseTemperature();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('decreases the temperature with the down button', function(){
+    thermostat.decreaseTemperature();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
 });
